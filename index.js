4aa7207eb7a7b70cfdb296f92988a6896e0a4f3c
nicomoret7 <nicomoret7@gmail.com>
2021-08-07 15:57:33 +0200

    comandos añadidos

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
 const Discord = require('discord.js');
-require('dotenv').config()
-
+require('dotenv').config();
 const client = new Discord.Client();
+const prefix = process.env.PREFIX;
+const fs = require('fs');
+
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+
+	client.commands.set(command.name, command);
+}
+
+client.once('ready', () => {
+	console.log('Raffle Bot is online!');
+});
+
+client.on('message', message => {
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).split(/ +/);
+	const command = args.shift().toLowerCase();
+
+	if (command === 'ping') client.commands.get('ping').execute(message, args);
+});
 
 client.login(process.env.DISCORD_TOKEN);
